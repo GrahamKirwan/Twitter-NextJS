632b093f77634055128a227a336d5e17b2179f91
Graham Kirwan <graham.kirwan4@gmail.com>
2022-05-18 18:57:34 -0400

    sidebar

---
+++ components/SidebarRow.tsx
@@ -0,0 +1,17 @@
+import React, { SVGProps } from 'react'
+
+interface Props {
+    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+    title: string
+}
+
+function SidebarRow({Icon, title}: Props) {
+  return (
+    <div className='group max-w-fit flex items-center rounded-full space-x-2 px-4 py-3 hover:bg-gray-100 cursor-pointer transition-all duration-200'>
+        <Icon className='h-6 w-6' />
+        <p className='group-hover:text-twitter'>{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
